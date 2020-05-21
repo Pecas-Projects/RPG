f1478af7304945244dde8a83217047a0dface183
fernandanlisboa <50326541+fernandanlisboa@users.noreply.github.com>
2020-05-21 19:53:31 -0300

    mapeamento de linhas e colunas feito na classe do Labirinto2

--- RPG/MeuRPGZinCore/Labirinto2.cs
+++ RPG/MeuRPGZinCore/Labirinto2.cs
@@ -19,12 +19,12 @@ namespace MeuRPGZinCore
             moedas.Add(new Moeda { X = 9, Y = 5 });
 
             //linha 1
-            linha1.Add(new Parede { topo = true, esquerda = true });
+            linha1.Add(new Parede { topo = true });
             linha1.Add(new Parede { topo = true, direita = true });
             linha1.Add(new Parede { esquerda = true, baixo = true });
             linha1.Add(new Parede { topo = true, baixo = true });
             linha1.Add(new Parede { topo = true, baixo = true });
-            linha1.Add(new Parede { topo = true});
+            linha1.Add(new Parede { topo = true, direita = true });
             linha1.Add(new Parede { topo = true, esquerda = true, baixo = true });
             linha1.Add(new Parede { topo = true, baixo = true });
             linha1.Add(new Parede { topo = true, direita = true }); //linha coordenada da saida
@@ -34,62 +34,89 @@ namespace MeuRPGZinCore
             linha2.Add(new Parede { direita = true, esquerda = true });
             linha2.Add(new Parede { topo = true, esquerda = true });
             linha2.Add(new Parede { topo = true, direita = true });
-            linha2.Add(new Parede { topo = true, esquerda = true, baixo = true});
-            linha2.Add(new Parede () ); //livre
-            linha2.Add(new Parede {topo = true, direita = true });
-            linha2.Add(new Parede {topo = true, esquerda = true });
-            linha2.Add(new Parede { direita = true});
+            linha2.Add(new Parede { topo = true, esquerda = true, baixo = true });
+            linha2.Add(new Parede()); //livre
+            linha2.Add(new Parede { topo = true, direita = true });
+            linha2.Add(new Parede { topo = true, esquerda = true });
+            linha2.Add(new Parede { direita = true });
 
             //linha3
-            linha3.Add(new Parede { direita = true, esquerda = true, baixo = true});
+            linha3.Add(new Parede { direita = true, esquerda = true, baixo = true });
             linha3.Add(new Parede { direita = true, esquerda = true });
             linha3.Add(new Parede { direita = true, esquerda = true });
-            linha3.Add(new Parede { esquerda = true, baixo = true});
-            linha3.Add(new Parede { topo =true, direita = true });
-            linha3.Add(new Parede {esquerda = true, direita = true });
-            linha3.Add(new Parede {esquerda = true, direita = true });
-            linha3.Add(new Parede {esquerda = true, direita = true });
+            linha3.Add(new Parede { esquerda = true, baixo = true });
+            linha3.Add(new Parede { topo = true, direita = true });
+            linha3.Add(new Parede { esquerda = true, direita = true });
+            linha3.Add(new Parede { esquerda = true, direita = true });
+            linha3.Add(new Parede { esquerda = true, direita = true });
             linha3.Add(new Parede { direita = true, baixo = true });
 
             //linha4
-            linha4.Add(new Parede { topo = true, esquerda = true});
+            linha4.Add(new Parede { topo = true, esquerda = true });
             linha4.Add(new Parede { direita = true, baixo = true });
-            linha4.Add(new Parede { direita = true, esquerda = true});
-            linha3.Add(new Parede { topo = true, esquerda = true, direita = true});
-            linha4.Add(new Parede { direita = true, esquerda = true});
-            linha4.Add(new Parede { esquerda = true, direita = true});
-            linha4.Add(new Parede { esquerda = true, baixo = true});
-            linha4.Add(new Parede { baixo = true});
-            linha4.Add(new Parede { topo = true, direita =true });
+            linha4.Add(new Parede { direita = true, esquerda = true });
+            linha3.Add(new Parede { topo = true, esquerda = true, direita = true });
+            linha4.Add(new Parede { direita = true, esquerda = true });
+            linha4.Add(new Parede { esquerda = true, direita = true });
+            linha4.Add(new Parede { esquerda = true, baixo = true });
+            linha4.Add(new Parede { baixo = true });
+            linha4.Add(new Parede { topo = true, direita = true });
 
             //linha 5
             linha5.Add(new Parede { direita = true, esquerda = true });
-            linha5.Add(new Parede { topo = true, baixo = true, esquerda = true});
-            linha5.Add(new Parede { baixo = true});
-            linha5.Add(new Parede { direita = true});
-            linha5.Add(new Parede { esquerda =true, baixo=true});
-            linha5.Add(new Parede {direita =true, baixo=true });
-            linha5.Add(new Parede { topo =true, esquerda=true});
-            linha5.Add(new Parede { topo=true, direita= true });
-            linha5.Add(new Parede { esquerda=true, direita =true});
+            linha5.Add(new Parede { topo = true, baixo = true, esquerda = true });
+            linha5.Add(new Parede { baixo = true });
+            linha5.Add(new Parede { direita = true });
+            linha5.Add(new Parede { esquerda = true, baixo = true });
+            linha5.Add(new Parede { direita = true, baixo = true });
+            linha5.Add(new Parede { topo = true, esquerda = true });
+            linha5.Add(new Parede { topo = true, direita = true });
+            linha5.Add(new Parede { esquerda = true, direita = true });
 
             //linha 6
-            linha6.Add(new Parede {direita=true, baixo=true });
-            linha6.Add(new Parede { topo =true, direita=true});
-            linha6.Add(new Parede { topo=true, direita=true, esquerda=true});
-            linha6.Add(new Parede { esquerda=true, direita=true });
-            linha6.Add(new Parede { topo = true, direita=true});
-            linha6.Add(new Parede { topo= true, baixo=true});
-            linha6.Add(new Parede { baixo= true, direita=true });
-            linha6.Add(new Parede { esquerda = true, baixo=true });
-            linha6.Add(new Parede { baixo = true, direita=true});
+            linha6.Add(new Parede { esquerda = true, baixo = true });
+            linha6.Add(new Parede { topo = true, direita = true });
+            linha6.Add(new Parede { topo = true, direita = true, esquerda = true });
+            linha6.Add(new Parede { esquerda = true, direita = true });
+            linha6.Add(new Parede { topo = true, esquerda = true });
+            linha6.Add(new Parede { topo = true, baixo = true });
+            linha6.Add(new Parede { baixo = true, direita = true });
+            linha6.Add(new Parede { esquerda = true, baixo = true });
+            linha6.Add(new Parede { baixo = true, direita = true });
 
             //linha 7
-            linha7.Add(new Parede { topo = true, esquerda=true, direita=true});
-            linha7.Add(new Parede { esquerda=true, baixo = true});
+            linha7.Add(new Parede { topo = true, esquerda = true, direita = true });
+            linha7.Add(new Parede { esquerda = true, baixo = true });
             linha7.Add(new Parede { }); //livre
-            linha7.Add(new Parede { direita = true, baixo=true});
+            linha7.Add(new Parede { direita = true, baixo = true });
+            linha7.Add(new Parede { esquerda = true, direita = true });
+            linha7.Add(new Parede { esquerda = true, topo = true });
+            linha7.Add(new Parede { topo = true, direita = true });
+            linha7.Add(new Parede { esquerda = true, topo = true });
+            linha7.Add(new Parede { topo = true, direita = true });
 
+            //linha 8 
+            linha8.Add(new Parede { direita = true });
+            linha8.Add(new Parede { topo = true, baixo = true });
+            linha8.Add(new Parede { baixo = true, direita = true });
+            linha8.Add(new Parede { esquerda = true, topo = true, direita = true });
+            linha8.Add(new Parede { esquerda = true, direita = true });
+            linha8.Add(new Parede { esquerda = true });
+            linha8.Add(new Parede { baixo = true });
+            linha8.Add(new Parede { baixo= true, direita = true });
+            linha8.Add(new Parede { esquerda = true, direita = true });
+
+
+            //linha9
+            linha9.Add(new Parede { esquerda = true, baixo = true, direita = true });
+            linha9.Add(new Parede { esquerda = true, topo = true, baixo = true });
+            linha9.Add(new Parede { topo = true, baixo = true });
+            linha9.Add(new Parede { baixo = true });
+            linha9.Add(new Parede { baixo = true });
+            linha9.Add(new Parede { baixo = true });
+            linha9.Add(new Parede { topo = true, baixo = true });
+            linha9.Add(new Parede { topo = true, baixo = true });
+            linha9.Add(new Parede { baixo = true });
 
         }
 
