227fe68fb7b3a03df3cdb7ac164bb0cb9b4adcc1
Ana Tartari <anatartari412@gmail.com>
2020-05-22 16:57:47 -0300

    Teste Ataque

--- RPG/MeuRPGZinTest/PersonagemTest.cs
+++ RPG/MeuRPGZinTest/PersonagemTest.cs
@@ -12,25 +12,32 @@ namespace MeuRPGZinTest
         [SetUp]
         public void Setup()
         {
-            Atacante = new Feiticeira { Forca = 10, PerdaEstamina = 0.2, GanhoEstamnina = 0.1, Escudo = 50};
-            Inimigo = new Feiticeira { Forca = 10, PerdaEstamina = 0.2, GanhoEstamnina = 0.1, Escudo = 50 };
-            /* p = new SereianosNPC { Forca = 20, PerdaEstamina = 0.25,
-                 GanhoEstamnina = 0.15, Escudo = 50};
-             ini = new Feiticeira {
-                 Forca = 20,
-                 PerdaEstamina = 0.25,
-                 GanhoEstamnina = 0.15,
-                 Escudo = 50
-             } ;*/
-            // p = new Personagem { Vida = 10, Nivel = 1 };            
+            Atacante = new Feiticeira { Forca = 20, PerdaEstamina = 0.2, GanhoEstamnina = 0.1, Escudo = 50};
+            Inimigo = new Feiticeira { Forca = 20, PerdaEstamina = 0.2, GanhoEstamnina = 0.1, Escudo = 50 };        
         }
 
         [Test]
         public void Ataque_SemEscudoAtivo()
         {
             Atacante.atacar(Inimigo);
-            Assert.AreEqual(Inimigo.Vida == 90, Atacante.Estamina == 0.8);
+            Assert.AreEqual(Inimigo.Vida == 80, Atacante.Estamina == 0.8);
             
+        }
+
+        [Test]
+        public void Ataque_EscudoAtivoEDanoNegativo() 
+        { 
+            Inimigo.usarEscudo();
+            Atacante.atacar(Inimigo);
+            Assert.AreEqual(Inimigo.Escudo == 43, Atacante.Estamina == 0.8);
+        }
+
+        [Test]
+        public void Ataque_EscudoAtivoEDanoPositivo()
+        {
+            Inimigo.usarEscudo();
+            Inimigo.Escudo = 15;
+            Assert.AreEqual(Inimigo.Vida == 95, Inimigo.Escudo == 45);
         }
     }
 }
