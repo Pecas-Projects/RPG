5c63cb71a11f51bc61335b90ab4e4d00214bf98a
Ana Tartari <anatartari412@gmail.com>
2020-05-28 22:56:52 -0300

    Remover e Add Whey

--- RPG/MeuRPGZinUWP/TelaIntegracao.xaml.cs
+++ RPG/MeuRPGZinUWP/TelaIntegracao.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=234238
@@ -58,76 +59,33 @@ namespace MeuRPGZinUWP
             this.Frame.Navigate(typeof(Loja), feiticeira);
         }
 
-        private void Levar_Pirlimpimpim(object sender, RoutedEventArgs e)
+        private void LevarWhey(object sender, RoutedEventArgs e)
         {
-           
-           feiticeira.EscolherItemdeBatalha(feiticeira.mochila.bagPirlimpimpim); 
-           selectPirlimpimpim.Content = "Retirar da mochila";
-          /*  if (feiticeira.ItemdeBatalha.Count == 1)
+            feiticeira.EscolherItemdeBatalha(feiticeira.mochila.bagWhey);
+            if(Item_mochila.Source.Equals("Assets/interrogacao.png"))
             {
-                Pirlimpimpim_Mochila.Width = 162;
-                Pirlimpimpim_Mochila.Height = 76;
+                //Mudar a imagem para Whey
             }
-            else if (feiticeira.ItemdeBatalha.Count == 2)
+            else if (Item_mochila2.Source.Equals("Assets/interrogacao.png"))
             {
-                Pirlimpimpim_Mochila2.Width = 162;
-                Pirlimpimpim_Mochila2.Height = 76;
-            }*/
-        }
-
-        private void Levar_Whey(object sender, RoutedEventArgs e)
-        {
-            if(selectWhey.Content.Equals("Levar para a batalha"))
-            {
-                contWhey.Text = "Poção Whey: " + (feiticeira.mochila.bagWhey.Count- 1) ;
-                selectWhey.Content = "Retirar da mochila";
-                feiticeira.EscolherItemdeBatalha(feiticeira.mochila.bagWhey);
-               /* if (feiticeira.ItemdeBatalha.Count == 1)
-                {
-                    Whey_Mochila.Width = 162;
-                    Whey_Mochila.Height = 76;
-                }
-                else if (feiticeira.ItemdeBatalha.Count == 2)
-                {
-                    Whey_Mochila2.Width = 162;
-                    Whey_Mochila2.Height = 76;
-                }
+                //Mudar imagem para Whey
             }
-            else
+
+        }
+
+        private void RetirarWhey(object sender, RoutedEventArgs e)
+        {
+            PocaoWhey whey = new PocaoWhey();
+            feiticeira.RetirarItemdeBatalha(feiticeira.mochila.bagWhey, whey);
+            if (Item_mochila2.Source.Equals("Assets/pocao_whey.png"))
             {
-                contWhey.Text = "Poção Whey: " + feiticeira.mochila.bagWhey.Count;
-                selectWhey.Content = "Levar para a batalha";
-                feiticeira.EscolherItemdeBatalha(feiticeira.mochila.bagWhey);
-                if (feiticeira.ItemdeBatalha[0] == feiticeira.mochila.bagWhey)
-                {
-                    Whey_Mochila.Width = 1;
-                    Whey_Mochila.Height = 1;
-                }
-                else if (feiticeira.ItemdeBatalha[1] == feiticeira.mochila.bagWhey)
-                {
-                    Whey_Mochila2.Width = 1;
-                    Whey_Mochila2.Height = 1;
-                }*/
+                //Mudar imagem para "?"
             }
-           
+            else if (Item_mochila.Source.Equals("Assets/pocao_whey.png"))
+            {
+                //Mudar imagem para "?"
+            }
         }
 
-        private void Levar_Fortalecedora(object sender, RoutedEventArgs e)
-        {
-            selectFortalecedora.Content = "Retirar da mochila";
-            feiticeira.EscolherItemdeBatalha(feiticeira.mochila.bagFortalecedora);
-        }
-
-        private void Levar_Radix(object sender, RoutedEventArgs e)
-        {
-            selectRadix.Content = "Retirar da mochila";
-            feiticeira.EscolherItemdeBatalha(feiticeira.mochila.bagRadix);
-        }
-
-        private void Levar_Vitae(object sender, RoutedEventArgs e)
-        {
-            selectVitae.Content = "Retirar da mochila";
-            feiticeira.EscolherItemdeBatalha(feiticeira.mochila.bagVitae);
-        }
     }
 }
