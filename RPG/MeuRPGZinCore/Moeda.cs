6052c07bddadf50eb4bda11d34170cfbc9f0dc99
AmadoMaria <mariantoniamado@hotmail.com>
2020-06-05 11:00:39 -0300

    Comentário na classe Moeda

--- RPG/MeuRPGZinCore/Moeda.cs
+++ RPG/MeuRPGZinCore/Moeda.cs
@@ -5,11 +5,12 @@ using System.Text;
 
 namespace MeuRPGZinCore
 {
+    /// <summary>
+    /// Classe que define as coordenadas de uma Moeda em um labirinto.
+    /// </summary>
     public class Moeda
     {
         public int X { get; set; }
         public int Y { get; set; }
-
-
     }
 }
