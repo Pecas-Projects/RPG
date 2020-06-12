988aac6c77eb01ba302d67154e6d2e4fc9a52fca
Ana Tartari <anatartari412@gmail.com>
2020-06-12 16:50:27 -0300

    Ataque especial e inteligencia Humano

--- RPG/MeuRPGZinCore/HumanoNPC.cs
+++ RPG/MeuRPGZinCore/HumanoNPC.cs
@@ -5,6 +5,7 @@ using System.Text;
 namespace MeuRPGZinCore
 {
     /// <summary>
+    /// Esperto, maligno, toxico e opressor (lixo em forma de ser vivo)
     ///Implementa a classe abstrata "Personagem" e a interface "PersonagemNPC".
     /// </summary>
     public class HumanoNPC : Personagem, PersonagemNPC
@@ -56,8 +57,161 @@ namespace MeuRPGZinCore
 
         public int Inteligencia(Feiticeira inimiga)
         {
-            //fazer
-            return 9999;
+            //Sorteia os numeros para a inteligencia 
+            Random radNum = new Random();
+            int dado = radNum.Next(9);
+            int decisao = radNum.Next(2);
+
+
+            //Caso entre neste if ele ira preferir DEFENDER
+            if (this.Estamina <= inimiga.Estamina && this.Vida < inimiga.Vida)
+            {
+                //Chance de 70% de defender
+                if (dado >= 0 && dado < 7)
+                {
+                    //Verifica se ele tem escudo
+                    if (this.Escudo > 0)
+                    {
+                        UsarEscudo();
+                        return 0;
+                    }
+                    //Verifica se é vantajoso atacar
+                    else if (this.Estamina >= 0.7)
+                    {
+                        if (decisao == 0 || decisao == 1)
+                        {
+                            Atacar(inimiga);
+                            return 1;
+                        }
+                        else
+                        {
+                            Descansar();
+                            return -1;
+                        }
+                    }
+                    //Caso nao tenha nenhuma alternativa
+                    else
+                    {
+                        Descansar();
+                        return -1;
+                    }
+                }
+                //Chance de Ataque de 30%
+                else
+                {
+                    //verifica se ele tem estamina
+                    if (this.Estamina >= this.PerdaEstamina)
+                    {
+                        Atacar(inimiga);
+                        return 1;
+                    }
+                    else
+                    {
+                        Descansar();
+                        return -1;
+                    }
+                }
+
+            }
+            //Caso entre neste if ele prefere atacar
+            else if (this.Estamina > inimiga.Estamina && this.Vida >= inimiga.Vida)
+            {
+                //cance de 90% de ataque
+                if (dado >= 0 && dado < 9)
+                {
+                    //verifica estamina
+                    if (this.Estamina >= this.PerdaEstamina)
+                    {
+                        Atacar(inimiga);
+                        return 1;
+                    }
+                    else
+                    {
+                        //decide entre defender e descansar
+                        if (decisao == 0 || decisao == 1)
+                        {
+                            if (this.Escudo > 0)
+                            {
+                                UsarEscudo();
+                                return 0;
+                            }
+                            else
+                            {
+                                Descansar();
+                                return -1;
+                            }
+                        }
+                        else
+                        {
+                            Descansar();
+                            return -1;
+                        }
+                    }
+                }
+                // Chance de usar escudo de 10% 
+                else
+                {
+                    if (this.Escudo > 0)
+                    {
+                        UsarEscudo();
+                        return 0;
+                    }
+                    else
+                    {
+                        Descansar();
+                        return -1;
+                    }
+                }
+
+            }
+            // Caso nao se encaixe em nenhuma ocasião especifica entra neste else
+            else
+            {
+                //Chance de usar escudo de 30%
+                if (dado >= 0 && dado <= 3)
+                {
+                    //verifica se ele tem escudo
+                    if (this.Escudo > 0)
+                    {
+                        UsarEscudo();
+                        return 0;
+                    }
+                    //caso nao tenha ele verifica a estamina e ataca
+                    else if (this.Estamina >= this.PerdaEstamina)
+                    {
+                        Atacar(inimiga);
+                        return 1;
+                    }
+                    else
+                    {
+                        Descansar();
+                        return -1;
+                    }
+                }
+                // Chance de atacar de 70%
+                else
+                {
+                    //verifica se ele tem estamina
+                    if (this.Estamina >= this.PerdaEstamina)
+                    {
+                        Atacar(inimiga);
+                        return 1;
+                    }
+                    else if (this.Escudo > 0)
+                    {
+                        UsarEscudo();
+                        return 0;
+                    }
+                    else
+                    {
+                        Descansar();
+                        return -1;
+                    }
+
+                }
+
+            }
+
         }
     }
 }
